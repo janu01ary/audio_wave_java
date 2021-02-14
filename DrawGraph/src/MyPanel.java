94196f625435f35b6f5a74d990093381746a6ee7
aaa <janu01ary@gmail.com>
2021-02-15 00:19:45 +0900

    그래프 그리기 완성

--- DrawGraph/src/MyPanel.java
+++ DrawGraph/src/MyPanel.java
@@ -1,19 +1,43 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Panel;
 
 public class MyPanel extends Panel {
 
-	private int x, y;
-	private int zoom;
+	private int x = 580, y = 520, zoom = 30;
+	private double incX = 0.1, minX = -10, maxX = 10;
+	private MyGraph graph = null;
 
 	public MyPanel() {
-		x = 580;
-		y = 520;
-		zoom = 30;
 		this.setSize(580, 520);
 	}
 	
+	public void setGraph(MyGraph g) {
+		graph = g;
+	}
+	
 	public void paint(Graphics g) {
+		this.drawAxis(g);
+		this.drawGraph(g);
+	}
+	
+	public void drawGraph(Graphics g) {
+		if (graph == null)	return;
+		
+		double resX = minX;
+		double resY = graph.f(resX);
+		for (double d = minX + incX; d < maxX; d += incX) {
+			g.setColor(Color.BLUE);
+			g.drawLine((int)(resX * zoom + x/2),
+						(int)(y/2 - resY * zoom),
+						(int)(d * zoom + x/2),
+						(int)(y/2 - graph.f(d) * zoom));
+			resX = d;
+			resY = graph.f(d);
+		}
+	}
+	
+	public void drawAxis(Graphics g) {
 		//draw edge line
 		g.drawLine(0, 0, 0, y);
 		g.drawLine(0, 0, x, 0);
@@ -23,13 +47,13 @@ public class MyPanel extends Panel {
 		//x line, y line
 		g.drawLine(0, y/2, x, y/2);
 		g.drawLine(x/2, 0, x/2, y);
-		
+				
 		//x, y arrow
 		g.drawLine(x - 5, y/2 - 5, x, y/2);
 		g.drawLine(x - 5, y/2 + 5, x, y/2);
 		g.drawLine(x/2 - 5, 5, x/2, 0);
 		g.drawLine(x/2 + 5, 5, x/2, 0);
-		
+				
 		//0, x, y
 		g.drawString("0", x/2 + 2, y/2 + 12);
 		g.drawString("x", x - 14, y/2 + 12);
